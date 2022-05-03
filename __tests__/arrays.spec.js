2f3269fd7e1f2920f528a979dd9e36831674a189
Raul <you@example.com>
2022-05-03 20:52:10 +0200

    started-code complete

--- __tests__/arrays.spec.js
+++ __tests__/arrays.spec.js
@@ -1,4 +1,7 @@
-import {capitalizeArrays,calcFinalGrade} from '../src/arrays.js';
+import {capitalizeArrays,
+    calcFinalGrade,
+    calcAverageCalories
+} from '../src/arrays.js';
 
 /* Dada un array de nombre de ciudades, devolver un array con la primera letra de cada ciudad en mayúsculas.
 
@@ -7,21 +10,33 @@ import {capitalizeArrays,calcFinalGrade} from '../src/arrays.js';
 test('1+1 = 2',()=>expect(1+1).toBe(2))
 
 describe('array maps',()=>{
-    test('capitalizeArrays() should return an array with capitalized names from city array',()=>{
-        const cities = ["miami", "barcelona", "madrid"];
-        const capitalizedCities = ["Miami", "Barcelona", "Madrid"];
+test('capitalizeArrays() should return an array with capitalized names from city array',()=>{
+    const cities = ["miami", "barcelona", "madrid"];
+    const capitalizedCities = ["Miami", "Barcelona", "Madrid"];
 
-        expect(capitalizeArrays(cities)).toMatchObject(capitalizedCities)
-    })
-    test('calcFinalGrade() should return an objects array with "name" and "final_grade"',()=>{
-        const students = [ {
-            name: "A",
-            firstProject: 82,
-            secondProject: 92,
-            finalExam: 70
-          }]
-          const finalGrade =calcFinalGrade(students);
-        expect(finalGrade).toMatchObject([{name:"A",final_grade:0.4*((82+92)/2)+(0.6*70)}])
-    })
+    expect(capitalizeArrays(cities)).toMatchObject(capitalizedCities)
+})
+test('calcFinalGrade() should return an objects array with "name" and "final_grade"',()=>{
+    const students = [ {
+        name: "A",
+        firstProject: 82,
+        secondProject: 92,
+        finalExam: 70
+      }]
+      const finalGrade =calcFinalGrade(students);
+    expect(finalGrade).toMatchObject([{name:"A",final_grade:0.4*((82+92)/2)+(0.6*70)}])
+})
+})
+
+describe('array reduce',()=>{
+ test('calcAverageCalories() should return the calculated average of calories',()=>{
+    let menu = [
+        { name: "a", calories: 100 },
+        { name: "b", calories: 200 },
+        { name: "c", calories: 300 },
+      ];
+
+    expect(calcAverageCalories(menu)).toBe(200);
  })
+})
 
